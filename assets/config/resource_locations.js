1ed3e56ae8413868e33fa740b3864feebfe92189
Eric Carty-Fickes <ericcf@northwestern.edu>
2013-07-24 16:05:46 -0500

    temporarily use local vm

--- assets/config/resource_locations.js
+++ assets/config/resource_locations.js
@@ -4,7 +4,8 @@ define(function() {
       urlRoot: "mock_data/"
     },
     production: {
-      urlRoot: "http://dashboard.cbits.northwestern.edu/umb/"
+      //urlRoot: "http://dashboard.cbits.northwestern.edu/umb/"
+      urlRoot: "http://192.168.56.101/umb/"
     }
   };
 });
