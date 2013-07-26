cf435120bd2f333d6944a6223faf9f65c848e38a
Eric Carty-Fickes <ericcf@northwestern.edu>
2013-07-26 10:56:57 -0500

    fix

--- medactive/models/med_prompt_response.py
+++ medactive/models/med_prompt_response.py
@@ -25,8 +25,8 @@ class MedPromptResponseManager(models.Manager):
     return result_list
 
   def _db_cursor(self, participant_id):
-    db_string = 'host=\'%(HOST)s\' dbname=\'' + self._db_name(participant_id) + \
-      '\' user=\'%(USER)s\' password=\'%(PASSWORD)s\'' % settings.PARTICIPANT_DB
+    db_string = ('host=\'%(HOST)s\' dbname=\'' + self._db_name(participant_id) + \
+      '\' user=\'%(USER)s\' password=\'%(PASSWORD)s\'') % settings.PARTICIPANT_DB
     connection = psycopg2.connect(db_string)
     
     return connection.cursor()
