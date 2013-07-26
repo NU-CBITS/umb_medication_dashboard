7305b03dfc09a3c061ba415ae947f3ca92cc280f
Eric Carty-Fickes <ericcf@northwestern.edu>
2013-07-26 15:22:24 -0500

    refactor

--- medactive/models/med_prompt_response.py
+++ medactive/models/med_prompt_response.py
@@ -1,30 +1,5 @@
 from django.db import models
-from medactive.models.db import participant_db_cursor
-
-def select_sql(model, cursor):
-  import re
-  cursor.execute('SELECT column_name FROM information_schema.columns WHERE table_name=\'%s\';' % model._meta.db_table)
-  db_col_names = list(name[0] for name in cursor.fetchall())
-  def alias(name):
-    if 'FEATURE_VALUE_DT_' + name in db_col_names:
-      return '"FEATURE_VALUE_DT_%s" AS "%s"' % (name, name)
-    return '"%s"' % name
-  cols = (alias(name) for name in model._meta.get_all_field_names())
-
-  return 'SELECT %s FROM "%s";' % (', '.join(cols), model._meta.db_table)
-
-class MedPromptResponseManager(models.Manager):
-  def all_for_participant(self, participant_id):
-    cursor = participant_db_cursor(participant_id)
-    sql = select_sql(self.model, cursor)
-    cursor.execute(sql)
-    desc = cursor.description
-    result_list = [
-      self.model(**dict(zip([col[0] for col in desc], row)))
-      for row in cursor.fetchall()
-    ]
-
-    return result_list
+from medactive.models.participant_model_manager import ParticipantModelManager
 
 class MedPromptResponse(models.Model):
   id = models.TextField(primary_key=True)
@@ -36,7 +11,7 @@ class MedPromptResponse(models.Model):
   reason_for_missing = models.TextField()
   doseTime = models.TextField()
 
-  objects = MedPromptResponseManager()
+  objects = ParticipantModelManager()
 
   class Meta:
     db_table = 'medication_survey_responses'
---
+++ medactive/models/participant_model_manager.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+import psycopg2
+
+class ParticipantModelManager(models.Manager):
+  def all_for_participant(self, participant_id):
+    cursor = self.participant_db_cursor(participant_id)
+    sql = self.select_sql(cursor)
+    cursor.execute(sql)
+    desc = cursor.description
+    result_list = [
+      self.model(**dict(zip([col[0] for col in desc], row)))
+      for row in cursor.fetchall()
+    ]
+
+    return result_list
+
+  def participant_db_cursor(self, participant_id):
+    db_string = ('host=\'%(HOST)s\' dbname=\'' + self._db_name(participant_id) + \
+      '\' user=\'%(USER)s\' password=\'%(PASSWORD)s\'') % settings.PARTICIPANT_DB
+    connection = psycopg2.connect(db_string)
+    
+    return connection.cursor()
+
+  def select_sql(self, cursor):
+    col_name_query = 'SELECT column_name FROM information_schema.columns WHERE table_name=\'%s\';' % \
+      self.model._meta.db_table
+    cursor.execute(col_name_query)
+    db_col_names = list(name[0] for name in cursor.fetchall())
+    def alias(name):
+      if 'FEATURE_VALUE_DT_' + name in db_col_names:
+        return '"FEATURE_VALUE_DT_%s" AS "%s"' % (name, name)
+      return '"%s"' % name
+    cols = (alias(name) for name in model._meta.get_all_field_names())
+
+    return 'SELECT %s FROM "%s";' % (', '.join(cols), model._meta.db_table)
+
+  def _db_name(self, participant_id):
+    return 'umb_' + participant_id
