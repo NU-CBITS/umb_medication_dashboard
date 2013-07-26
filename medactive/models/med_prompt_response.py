704fc6959c3430a5bf9c292f9d7416430b3e9020
Eric Carty-Fickes <ericcf@northwestern.edu>
2013-07-26 13:13:22 -0500

    testing

--- medactive/models/med_prompt_response.py
+++ medactive/models/med_prompt_response.py
@@ -24,14 +24,14 @@ class MedPromptResponseManager(models.Manager):
     return result_list
 
 class MedPromptResponse(models.Model):
-  id = models.TextField(primary_key=True)
-  eventDateTime = models.DateTimeField()
-  surveyVersion = models.BigIntegerField()
-  timeStarted = models.TextField()
-  index = models.TextField()
-  date = models.TextField()
-  reason_for_missing = models.TextField()
-  doseTime = models.TextField()
+  #id = models.TextField(primary_key=True)
+  #eventDateTime = models.DateTimeField()
+  #surveyVersion = models.BigIntegerField()
+  #timeStarted = models.TextField()
+  #index = models.TextField()
+  #date = models.TextField()
+  #reason_for_missing = models.TextField()
+  #doseTime = models.TextField()
 
   objects = MedPromptResponseManager()
 
