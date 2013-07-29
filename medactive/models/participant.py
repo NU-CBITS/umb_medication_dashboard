1712c15054d09703dcadd5efb2fc96f6b16f2a28
Eric Carty-Fickes <ericcf@northwestern.edu>
2013-07-29 12:50:10 -0500

    fix typo

--- medactive/models/participant.py
+++ medactive/models/participant.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Participant(models.Model):
-  participant_id = models.Charfield(max_length=255)
+  participant_id = models.CharField(max_length=255)
 
   class Meta:
     app_label = 'medactive'
